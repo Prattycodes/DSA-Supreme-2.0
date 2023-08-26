570b71ad216b1167f1cafcd5b8cc7f79e040f973
Pratham Anil Tetgure <89979888+Prattycodes@users.noreply.github.com>
2023-08-26 11:39:14 +0530

    Lecture-Codes

---
+++ BasicOfProgramming-Level1/LectureNo2-FirstProgram/firstProgram.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // cout<<"Pratham Tetgure"<<endl;
+    // cout<<"Hello dunia"; cout<<"LOL"<<endl;
+
+    //int--> 4 Byte
+    // int num= 50;
+    // cout<<num<<endl;
+    // cout<<sizeof(num)<<endl;
+
+    //Character --> 1 byte
+    // char ch= 'k';
+    // cout<<ch<<endl;
+    // cout<<sizeof(ch)<<endl; 
+
+    //Float --> 4 Byte 
+    // float point= 1.72;
+    // cout<<point<<endl;
+    // cout<<sizeof(point)<<endl;
+    
+    //Long --> 4 Byte  
+    // long b= 229;
+    // cout<<b<<endl;
+    // cout<<sizeof(b)<<endl;
+
+    //Cin Operation 
+    // char ch;
+    // cin>>ch;
+    // cout<<ch<<endl;
+
+    //OR Operation
+    // int age = 21;
+	// int car = 0;
+	
+	// if(age >= 18 || car >=1) {
+	// 	cout << "License miljaega" << endl;
+	// }
+
+    //NOT Operation 
+    // int age= 0;
+    // cout << !age << endl;
+
+    //Equal to Operation 
+    // int a = 20;
+	// int b = 20;
+	// cout << (a == b) << endl;
+
+    //Arithmetic Operation 
+    // int a = 3;
+	// int b = 2;
+	// cout << a + b << endl;
+	// cout << a - b << endl;
+	// cout << a*b << endl;
+	// cout << a/b << endl;
+	// cout << b % a << endl; 
+
+    //Garbage value 
+    int num;
+    cout<<num<<endl;
+    return 0;
+}
