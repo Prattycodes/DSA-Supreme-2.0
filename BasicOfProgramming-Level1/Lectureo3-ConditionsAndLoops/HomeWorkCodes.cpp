23f4e44f13d1c83e18c987df2f7fa6ff3ca04a8f
Pratham Anil Tetgure <89979888+Prattycodes@users.noreply.github.com>
2023-09-02 22:31:13 +0530

    Class Codes

---
+++ BasicOfProgramming-Level1/Lectureo3-ConditionsAndLoops/HomeWorkCodes.cpp
@@ -0,0 +1,103 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    // //Question No 1
+    // int a, b;
+    // cin>>a>>b;
+    // int c= a*b;
+    // cout<<c<<endl;
+
+    // //Question No 2
+    // int a, b, c;
+    // cin>>a>>b>>c;
+    // int P= a+b+c;
+    // cout<<P<<endl;
+
+    // //Question No 3
+    // int P, N, R;
+    // cin>>P>>N>>R;
+    // int SI= (P+N+R)/100;
+    // cout<<SI<<endl;
+
+    // //Question No 4
+    // int P, R, N, T;
+    // cin>>P>>R>>N>>T;
+    // int CI= P*pow((1+(R/N)), N*T);
+    // cout<<CI<<endl;
+
+    //Question No 5
+    // int n; 
+    // cin>>n;
+    // for(int i= n; i>0; i--){
+    //     cout<<i<<endl;
+    // }
+
+    //Question No 6
+    // int n;
+    // cin>>n;
+    // int fact= 1;
+    // for(int i=n; i>0; i--){
+    //     fact *= i;
+    // }
+    // cout<<fact;
+
+    //Question No 7 
+    // int n;
+    // cin>>n;
+    // for(int i=2; i<n; i++){
+    //     if(n%i==0){
+    //         cout<<"Non-Prime";
+    //         return 0;
+    //     }
+    // }
+    // cout<<"Prime"<<endl;
+
+    //Question No 8 
+    // int a, b, c;
+    // cin>>a>>b>>c;
+    // if(a<b+c){
+    //     if(b<a+c){
+    //         if(c<b+a){
+    //             cout<<"Valid Triangle!!"<<endl;
+    //             return 0;
+    //         }
+    //     }
+    // }
+    // cout<<"Not a valid Triangle!!"<<endl;
+
+    //Question No 9 
+    // int a, b, c;
+    // cin>>a>>b>>c;
+    // int maxi= a;
+    // if(b>maxi){
+    //     maxi= b;
+    //     if(c>maxi){
+    //         maxi= c;
+    //     }
+    // }
+    // else if(c>maxi){
+    //     maxi= c;
+    // }
+    // cout<<maxi<<endl;
+
+    //Bonus Questions 
+    // int c;
+    // if(cin>>c){
+    //     cout<<"Hello Pratham !"<<endl;
+    // }
+    // else{
+    //     cout<<"Else case!!"<<endl;
+    // }
+    //Conclusion- Always If will work!!
+
+    // int c= -1;
+    // if(cout<<c){
+    //     cout<<"Hello Pratham !"<<endl;
+    // }
+    // else{
+    //     cout<<"Else case!!"<<endl;
+    // }
+    //Conclusion- Always If will work!!
+    return 0;
+}
